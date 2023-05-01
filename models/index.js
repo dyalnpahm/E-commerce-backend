83885cf958da7487393022b36cd7f805d68eb6c9
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-01 19:36:38 +1000

    index.js association

--- models/index.js
+++ models/index.js
@@ -5,13 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasmany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: productTag,
+  foreignKey : 'product_id'
+  });
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  through: productTag,
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
