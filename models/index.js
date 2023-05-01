b62bc1f8091fbbd4c21a1c432a3a418cb431a64a
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-01 20:13:43 +1000

    fixed syntax error with ProductTag

--- models/index.js
+++ models/index.js
@@ -14,12 +14,12 @@ Category.hasmany(Product,{
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
-  through: productTag,
+  through: ProductTag,
   foreignKey : 'product_id'
   });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongToMany(Product, {
-  through: productTag,
+  through: ProductTag,
   foreignKey: 'tag_id'
 });
 module.exports = {
