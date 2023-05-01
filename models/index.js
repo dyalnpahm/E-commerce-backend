3a3acadc37e08ca7685ecc0c206695389d4a3bd3
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-01 20:13:14 +1000

    added foreignKeys

--- models/index.js
+++ models/index.js
@@ -5,9 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-Category.hasmany(Product);
+Category.hasmany(Product,{
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through: productTag,
